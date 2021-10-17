e80e495bbe044d5a376642cea382a2d0fbea6c18
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-10-17 20:42:19 -0300

    exs 3 to 6

---
+++ learnyounode/filtered-ls.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const ext = '.' + process.argv[3]
+
+fs.readdir(process.argv[2], function (err, list) {
+    if(err) {
+        console.log(error);
+    }
+    list.forEach(function (file) {
+        if(path.extname(file) === ext) {
+            console.log(file);
+        }
+    })
+
+});
